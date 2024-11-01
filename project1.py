7a6f57b6c4889a6aece3fd697894fd7fed0f4302
Buğra Keser <162129134+bugrakeserr@users.noreply.github.com>
2024-11-01 22:34:39 +0300

    code uploaded

---
+++ project1.py
@@ -0,0 +1,71 @@
+import time
+import random
+
+def cmpe_algorithm(arr: list) -> int:
+    count = 0
+    n = len(arr)
+    start_time = time.time()
+    res = 1
+    for i in range(n):
+        if arr[i] == 'c':
+            for j in range(n, 0, -1):
+                k = i + n
+                for y in range(0, n+1, j):
+                    k = k - 1
+                    count +=1
+                res = res + k
+        elif arr[i] == 'm':
+            z = 1
+            while z < n:
+                z = z * 2
+                count +=1
+                res = res + z
+        elif arr[i] == 'p':
+            w = n
+            res = res - 1
+            while w > 0:
+                w = w // 5
+                count +=1
+                res = res + 1
+        elif arr[i] == 'e':
+            for m in range(1, i+1):
+                p = m
+                for l in range(m, n+1):
+                    for t in range(n, 0, -1):
+                        p = p + t
+                        count +=1
+                    res = res + p
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(execution_time)
+    print(count)
+    return res
+
+
+def array_generator(scenario, size):
+    arr = []
+    if scenario == 'worst':
+        arr.append('c')
+        for i in range(size-1):
+            arr.append('e')
+    elif scenario == 'best':
+        arr.append('e')
+        for i in range(size-1):
+            arr.append('p')
+    else:
+        letters = ["c","m","p","e"]
+        probs = [1/8,1/4,1/8,1/2]
+        arr = random.choices(letters,probs,k=size)
+    return arr
+ 
+
+scenario = input()
+size = int(input())
+
+
+arr = array_generator(scenario, size)
+arr[0] = 'e'
+print(arr)
+cmpe_algorithm(arr)
+
+
