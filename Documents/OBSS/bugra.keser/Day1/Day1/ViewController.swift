c9e653d4c1bf3b8987b1e0dcf47a8f403b9ec28f
Buğra Keser <bugra.keser@outlook.com.tr>
2025-07-23 12:04:39 +0300

    added Day1

---
+++ Documents/OBSS/bugra.keser/Day1/Day1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day1
+//
+//  Created by Bugra Keser on 22.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Documents/OBSS/bugra.keser/Day1/Day1Tests/Day1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Day1Tests.swift
+//  Day1Tests
+//
+//  Created by Bugra Keser on 22.07.2025.
+//
+
+import Testing
+@testable import Day1
+
+struct Day1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
